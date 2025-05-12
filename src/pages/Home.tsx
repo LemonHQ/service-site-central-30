336ae6df519b581b9f94c81cc9cead77d37f7917
lemonadvertising <development@lemonhq.io>
2025-05-12 11:29:11 +0100

    Homepage quote update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,8 +48,8 @@ const Home = () => {
       {/* High Impact Quote Section */}
       <QuotePanel 
         quote="Staying relevant requires unlearning what's outdated, co-creating what's next, and scaling what works"
-        author="Husain Mohsin"
-        authorTitle="Enterprise Partner"
+        author=""
+        authorTitle=""
         className="my-16"
       />
       
